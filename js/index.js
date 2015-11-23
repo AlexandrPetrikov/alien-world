8e74b139dd1db60131983eb25d69c2e7aa06d12d
AlexandrPetrikov <mojref@gmail.com>
2015-11-23 16:25:31 +0200

    us interface

--- js/index.js
+++ js/index.js
@@ -1,11 +1,30 @@
 $(document).ready(function(){
+
+$("input[type='submit']").click(function(){
+	$("#identification").hide();
+	return false;
+});
 /*--------------------scroll top --------------------*/
 $("#new-game").click(function(){
 	$("#identification").css("display","block");
-
 });
-/*----------------	-------------------------*/
+/*---------------- current hours-------------------------*/
+function clock(){
 
-
+var date = new Date(),
+    dateDate = date.getDate(),
+	dateMonth = date.getMonth(),
+	dateFullYear = date.getFullYear(),
+	dateHours = date.getHours(),
+	dateMinutes = date.getMinutes(),
+	dateSeconds = date.getSeconds(),
+	fullDate = dateDate + "." + dateMonth + "." + 
+			   dateFullYear + "  " + dateHours + ":" +
+			   dateMinutes + ":" + dateSeconds;
+$("#dateHours").text(dateHours < 10 ? "0"+dateHours : dateHours);
+$("#dateMinutes").text(dateMinutes < 10 ? "0"+dateMinutes : dateMinutes);
+$("#dateSeconds").text(dateSeconds < 10 ? "0"+dateSeconds : dateSeconds);
 };
+
+setInterval(clock,100);
 });
